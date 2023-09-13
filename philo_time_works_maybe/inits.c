7abac4abd1b9c8b8d62f707af60e0231ba4dce7f
Rin Sudz <100066318+madlfox7@users.noreply.github.com>
2023-09-13 13:21:23 +0400

    Add files via upload

---
+++ philo_time_works_maybe/inits.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inits.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudzilo <ksudzilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 14:29:13 by ksudzilo          #+#    #+#             */
+/*   Updated: 2023/09/13 13:14:15 by ksudzilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int	init_data(t_data *data, char **argv)
+{
+	get_time_in_ms();
+	data->die_time = ft_atoi(argv[2]);
+	data->count_philo = ft_atoi(argv[1]);
+	if (!argv[5])
+		data->max_eat = 0;
+	else
+		data->max_eat = ft_atoi(argv[5]);
+	data->is_die = 0;
+	pthread_mutex_init(&(data->m_printf), NULL);
+	pthread_mutex_init(&(data->m_die), NULL);
+	data->forks = (pthread_mutex_t *)
+		malloc(sizeof(pthread_mutex_t) * data->count_philo);
+	if (!data->forks)
+		return (-1);
+	data->philosophers = (t_sage *)
+		malloc(sizeof(t_sage) * data->count_philo);
+	if (!data->philosophers)
+		return (-1);
+	return (1);
+}
+
+void	fork_init(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->count_philo)
+	{
+		if (i == data->count_philo - 1)
+		{
+			data->philosophers[i].left_fork = &(data->forks[i]);
+			data->philosophers[i].right_fork = &(data->forks[0]);
+		}
+		else
+		{
+			data->philosophers[i].left_fork = &(data->forks[i]);
+			data->philosophers[i].right_fork = &(data->forks[i + 1]);
+		}
+		i++;
+	}
+}
+
+int	init_sage(t_data *data, char **argv)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->count_philo)
+	{
+		data->philosophers[i].id = i;
+		data->philosophers[i].eat_time = ft_atoi(argv[3]);
+		data->philosophers[i].sleep_time = ft_atoi(argv[4]);
+		data->philosophers[i].count_eat = 0;
+		data->philosophers[i].last_meal_time = 0;
+		data->philosophers[i].is_die = &(data->is_die);
+		data->philosophers[i].m_printf = &(data->m_printf);
+		data->philosophers[i].m_die = &(data->m_die);
+		pthread_mutex_init(&(data->philosophers[i].m_last_meal_time), NULL);
+		pthread_mutex_init(&(data->philosophers[i].m_count_eat), NULL);
+		pthread_mutex_init(&(data->forks[i]), NULL);
+		i++;
+	}
+	fork_init(data);
+	return (1);
+}
---
+++ philo_time_works_maybe/main.c
@@ -0,0 +1,97 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudzilo <ksudzilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/04 12:52:06 by ksudzilo          #+#    #+#             */
+/*   Updated: 2023/09/13 13:19:05 by ksudzilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+void	print(long long int time, t_sage *sage, char *s)
+{
+	pthread_mutex_lock(sage->m_printf);
+	if (!is_dead(sage))
+		printf("%lld %d %s", time, sage->id + 1, s);
+	pthread_mutex_unlock(sage->m_printf);
+}
+
+void	increment_count(t_sage *sage)
+{
+	pthread_mutex_lock(&(sage->m_count_eat));
+	sage->count_eat++;
+	pthread_mutex_unlock(&(sage->m_count_eat));
+}
+
+void	*woe_from_wit(void *arg)
+{
+	t_sage	*sage;
+
+	sage = (t_sage *)arg;
+	if (sage->id % 2 != 0)
+		usleep(1000);
+	while (!is_dead(sage))
+	{
+		pthread_mutex_lock(sage->left_fork);
+		print(get_time_in_ms(), sage, "has taken a fork\n");
+		pthread_mutex_lock(sage->right_fork);
+		print(get_time_in_ms(), sage, "has taken a fork\n");
+		print(get_time_in_ms(), sage, "is eating\n");
+		pthread_mutex_lock(&(sage->m_last_meal_time));
+		sage->last_meal_time = get_time_in_ms();
+		pthread_mutex_unlock(&(sage->m_last_meal_time));
+		ft_my_usleep(sage, sage->eat_time);
+		increment_count(sage);
+		pthread_mutex_unlock(sage->left_fork);
+		pthread_mutex_unlock(sage->right_fork);
+		print(get_time_in_ms(), sage, "is sleeping\n");
+		ft_my_usleep(sage, sage->sleep_time);
+		print(get_time_in_ms(), sage, "is thinking\n");
+	}
+	return (NULL);
+}
+
+void	fiat_philosophi_threads(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->count_philo)
+	{
+		pthread_create(&(data->philosophers[i].threads),
+			NULL, &woe_from_wit, (void *)&(data->philosophers[i]));
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+	int		i;
+
+	if (null_argv(argc, argv) || !check_args(argc, argv))
+		return (1);
+	if (init_data(&data, argv) == -1 || init_sage(&data, argv) == -1)
+	{
+		printf("Initialization error\n");
+		return (1);
+	}
+	fiat_philosophi_threads(&data);
+	while (1)
+	{
+		if (die_case(&data) == 1 || eat_case(&data) == 1)
+			break ;
+	}
+	i = 0;
+	while (i < data.count_philo && data.count_philo != 1)
+	{
+		pthread_join(data.philosophers[i].threads, NULL);
+		i++;
+	}
+	cleanup(&data);
+	return (0);
+}
